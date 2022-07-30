e761b5afb2768f88cf53ef2db74ff3b148d47ef9
newbalancer <pnw123456@icloud.com>
2022-07-30 23:55:52 +0900

    feat: effect item1 example add

---
+++ src/main/kotlin/effect/item1/Event.kt
@@ -0,0 +1,21 @@
+package effect.item1
+
+interface EventItem {
+    val active: Boolean
+}
+
+class ActionEvent(override var active: Boolean) : EventItem {
+}
+
+val name: String? = "Marton"
+
+val surname: String? = "Branum"
+
+val fullName: String?
+    get() = name?.let {
+        "$it $surname"
+    }
+
+val fullName2: String? = name?.let {
+    "$it $surname"
+}
---
+++ src/test/kotlin/effect/item1/ActionEventTest.kt
@@ -0,0 +1,30 @@
+package effect.item1
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+internal class ActionEventTest {
+
+    @Test
+    fun `인터페이스 프로퍼티 재정의 테스트 케이스`() {
+
+        val eventItem = ActionEvent(false)
+
+        eventItem.active = true
+
+        assertTrue(eventItem.active)
+    }
+
+    @Test
+    fun `델리게이트`() {
+
+        if (fullName != null) {
+            println(fullName!!.length)
+        }
+
+        if (fullName2 != null) {
+            println(fullName2!!.length)
+        }
+    }
+
+}
