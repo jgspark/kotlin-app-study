e60b65bdab5980079755ef5074fcf77667825fef
newbalancer <pnw123456@icloud.com>
2022-08-29 21:00:48 +0900

    feat: null safety ex

--- src/main/kotlin/basic/BaseUtil.kt
+++ src/main/kotlin/basic/BaseUtil.kt
@@ -5,3 +5,9 @@ enum class Flag {
 }
 
 fun isBigerInt(val1: Int) = if (val1 in 100..500) Flag.T else Flag.F
+
+fun test1(): String? {
+    var str: String? = "null"
+    str = null
+    return str
+}
